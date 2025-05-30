3c8d0729fd3e2d0c7fe682296015e31ab5008759
fersanchez1296 <fernandosanchezplacencia@gmail.com>
2025-05-30 15:58:56 -0600

    se agrego cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1");
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors({
+    origin: ['http://localhost:4000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4601);
 }
 bootstrap();   
